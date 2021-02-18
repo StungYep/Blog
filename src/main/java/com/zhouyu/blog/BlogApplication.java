71665dae9cd5775cd76786bedafb002364935220
Your Name <you@example.com>
2021-02-18 14:30:14 +0800

    init repo

---
+++ src/main/java/com/zhouyu/blog/BlogApplication.java
@@ -0,0 +1,15 @@
+package com.zhouyu.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class BlogApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext run = SpringApplication.run(BlogApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/zhouyu/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhouyu.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
