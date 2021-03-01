1243ad62d0c73795eecee6d785d7c2536733bd57
Your Name <you@example.com>
2021-03-01 21:50:59 +0800

    P37

---
+++ src/main/java/com/zhouyu/blog/service/CommentService.java
@@ -0,0 +1,10 @@
+package com.zhouyu.blog.service;
+
+import com.zhouyu.blog.model.Comment;
+import com.zhouyu.blog.model.User;
+
+public class CommentService {
+
+    public void insert(Comment comment, User user) {
+    }
+}
